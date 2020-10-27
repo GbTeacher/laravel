9436ddaf1d9e847b16786f88669466aa55f3a98e
Oleg Malinskiy <malinskii_o@tass.ru>
2020-10-27 22:34:02 +0300

    feat (blade) Lesson 3

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\InvokeController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\NewsController;
-use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,59 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('/home', function () {
-    echo "Hello from home!";
-})->name('home');
+Route::get('/category', [CategoryController::class, 'all'])->name('category');
 
-Route::get('/news', [NewsController::class, 'all'])->name('news');
+Route::get('/news/category/{categoryId}', [NewsController::class, 'allByCategory'])->name('category.news');
 
 Route::get('/news/{id}', [NewsController::class, 'one'])->name('news.id');
 
-//Route::resource('resource', ResourceController::class)->only([
-//    'index', 'show'
-//]);
-//
-//Route::resource('resource', ResourceController::class)->except([
-//    'index', 'show'
-//]);
-
-//Route::prefix('admin')->group(function () {
-//    Route::get('/', function () {
-//        echo 'Admin';
-//    });
-//
-//    Route::get('/panel', function () {
-//        echo 'Admin Panel';
-//    });
-//});
-
-//Route::namespace()
-//Route::name('admin.')->group(function () {
-//    Route::get('/', function () {
-//        echo 'Admin';
-//    })->name('index'); //admin.index
-//});
-//$url = \route('home');
-
-//Route::get($uri, $callback);
-//Route::post($uri, $callback);
-//Route::put($uri, $callback);
-//Route::patch($uri, $callback);
-//Route::delete($uri, $callback);
-//Route::options($uri, $callback);
-
-//Route::match(['get', 'post'], '/match', function () {});
-//Route::any('/', function () {});
-
-//Route::view('/', 'welcome');
-
-//Route::get('/news/{id?}', function ($id) {
-//    echo $id;
-//});
-
-//Route::fallback(function () {
-//    echo 'NOPE!';
-//});
 
